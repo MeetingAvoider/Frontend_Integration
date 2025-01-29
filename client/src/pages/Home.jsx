e495a6880caef8b0f853657df796663ac9b3f338
aadarsh <aadarshg087@gmail.com>
2025-01-29 18:59:32 +0530

    Bug Home API

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import Card from "../components/Card";
+import axios from "axios";
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`;
+
+const Home = ({ type }) => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const fetchVideos = async () => {
+      try {
+        const res = await axios.get(`http://localhost:8000/api/v1/videos/home`);
+        console.log(res.data);
+        console.log(res);
+        setVideos(res.data);
+      } catch (err) {
+        console.log(err);
+        console.log("asdfghjkl");
+      }
+    };
+    fetchVideos();
+  }, [type]);
+
+  return (
+    <Container>
+      {videos.map((video) => (
+        <Card key={video._id} video={video} />
+      ))}
+    </Container>
+  );
+};
+
+export default Home;
